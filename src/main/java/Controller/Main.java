fc66089a6f0d4b3317671abee66d82be57ded74c
dsblo <dsblo@kyodeux>
2024-05-27 18:48:28 -0500

    Model-View-Controller Setup 2

--- src/main/java/Controller/Main.java
+++ src/main/java/Controller/Main.java
@@ -1,16 +1,29 @@
 package Controller;
 
+import java.io.IOException;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage stage) throws Exception {
-        stage.show();
+    public void start(Stage stage){
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("/View/Main.fxml"));
+        
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+            
+        }catch(IOException e){}
     }
     
     public static void main(String[] args) {
         launch(args);
+        
+        
     }
 }
