2d79ed5024656dea1ace06691b7baf0308db9ba3
dsblo <dsblo@kyodeux>
2024-05-27 21:27:40 -0500

    Model-View-Controller Setup 3

---
+++ src/main/java/Controller/MainController.java
@@ -0,0 +1,11 @@
+package Controller;
+
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+
+public class MainController {
+    @FXML
+    public void onClick(ActionEvent event){
+        System.out.println("Hola mundo!");
+    }
+}
